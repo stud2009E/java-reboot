60379493da6bdbab2b3b14aadb49def0374001e8
unknown <stud2009@yandex.ru>
2023-11-04 15:53:06 +0300

    fix person toString method, add person example in App

--- module04/src/main/java/ru/sberbank/edu/App.java
+++ module04/src/main/java/ru/sberbank/edu/App.java
@@ -4,11 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class App {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         Integer[] data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         List<Integer> list = new ArrayList<>(List.of(data));
         list.sort(new CustomDigitComparator());
 
         System.out.println(list);
+
+        Person person = new Person("City", "name", 15);
+        System.out.println(person);
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Ccc", "aaa", 25));
+        personList.add(new Person("Bbb", "ddd", 26));
+        personList.add(new Person("Bbb", "zzz", 26));
+        personList.add(new Person("Aaa", "ccc", 27));
+        personList.add(new Person("Aaa", "ccc", 21));
+
+        personList.sort(Person::compareTo);
+
+        System.out.println(personList);
     }
 }
