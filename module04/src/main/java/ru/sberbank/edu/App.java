123a29cf737cb80aadca3f2c99ddf7412cb4474b
unknown <stud2009@yandex.ru>
2023-11-04 13:55:49 +0300

    add even/odd comparator + test

--- module04/src/main/java/ru/sberbank/edu/App.java
+++ module04/src/main/java/ru/sberbank/edu/App.java
@@ -1,13 +1,14 @@
 package ru.sberbank.edu;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main( String[] args ) {
+        Integer[] data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<Integer> list = new ArrayList<>(List.of(data));
+        list.sort(new CustomDigitComparator());
+
+        System.out.println(list);
     }
 }
