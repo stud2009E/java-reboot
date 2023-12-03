ed5e6408eba5e03e0bd86797eb1e0f0b3ac31a19
unknown <stud2009@yandex.ru>
2023-12-03 19:26:59 +0300

    refactor weather app with spring context

--- module09/src/main/java/ru/sberbank/edu/App.java
+++ module09/src/main/java/ru/sberbank/edu/App.java
@@ -1,13 +1,19 @@
 package ru.sberbank.edu;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        WeatherCache cache = context.getBean(WeatherCache.class);
+
+        WeatherInfo info = cache.getWeatherInfo("Paris");
+
+        System.out.println(info);
     }
 }
