27a5cb1e5004273f3ed244e2ad53a6e4122375f6
unknown <stud2009@yandex.ru>
2024-01-20 17:32:55 +0300

    fix: setup custom 404 Not found

--- module11/src/main/java/com/example/module11/config/Config.java
+++ module11/src/main/java/com/example/module11/config/Config.java
@@ -1,44 +1,10 @@
 package com.example.module11.config;
 
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-
-import javax.sql.DataSource;
 @EnableJpaRepositories(basePackages = "com.example.module11.dao")
 @Configuration
 @PropertySource("classpath:application.yml")
 public class Config {
-//    @Bean
-//    public DataSource dataSource(){
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//
-//        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
-//        dataSource.setDriverClassName("org.h2.Driver");
-//        dataSource.setUsername("sa");
-//        dataSource.setPassword("password");
-//
-//        return dataSource;
-//    }
-//    @Bean
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
-//        LocalContainerEntityManagerFactoryBean managerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-//        managerFactoryBean.setDataSource(dataSource());
-//        managerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-//
-//        managerFactoryBean.setPackagesToScan("com.example.module11");
-//
-//        return managerFactoryBean;
-//    }
-
-//    @Bean
-//    public JpaTransactionManager transactionManager(EntityManagerFactory emf){
-//        return new JpaTransactionManager(emf);
-//    }
 }
