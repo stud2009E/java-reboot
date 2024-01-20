68d3f2195c72e7f7521f6505d3cfc2f136ccaefc
unknown <stud2009@yandex.ru>
2024-01-20 17:00:21 +0300

    feature: add service for repo; change application settings to yaml

--- module11/src/main/java/com/example/module11/config/Config.java
+++ module11/src/main/java/com/example/module11/config/Config.java
@@ -13,32 +13,32 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import javax.sql.DataSource;
 @EnableJpaRepositories(basePackages = "com.example.module11.dao")
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:application.yml")
 public class Config {
-    @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//    @Bean
+//    public DataSource dataSource(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//
+//        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+//        dataSource.setDriverClassName("org.h2.Driver");
+//        dataSource.setUsername("sa");
+//        dataSource.setPassword("password");
+//
+//        return dataSource;
+//    }
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+//        LocalContainerEntityManagerFactoryBean managerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+//        managerFactoryBean.setDataSource(dataSource());
+//        managerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+//
+//        managerFactoryBean.setPackagesToScan("com.example.module11");
+//
+//        return managerFactoryBean;
+//    }
 
-        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("password");
-
-        return dataSource;
-    }
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
-        LocalContainerEntityManagerFactoryBean managerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        managerFactoryBean.setDataSource(dataSource());
-        managerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-
-        managerFactoryBean.setPackagesToScan("com.example.module11");
-
-        return managerFactoryBean;
-    }
-
-    @Bean
-    public JpaTransactionManager transactionManager(EntityManagerFactory emf){
-        return new JpaTransactionManager(emf);
-    }
+//    @Bean
+//    public JpaTransactionManager transactionManager(EntityManagerFactory emf){
+//        return new JpaTransactionManager(emf);
+//    }
 }
