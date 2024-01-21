94bba06c01c1ef9dd25d6ba58750f91947137062
unknown <stud2009@yandex.ru>
2024-01-21 14:52:24 +0300

    add user service tests, repository test

--- module11/src/main/java/com/example/module11/dao/repo/UserRepository.java
+++ module11/src/main/java/com/example/module11/dao/repo/UserRepository.java
@@ -4,7 +4,10 @@ import com.example.module11.dao.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+        List<UserEntity> findAllByNameOrderByAgeAsc(String name);
 }
