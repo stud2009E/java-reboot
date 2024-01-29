6f316aa41fa17e28064cebbe9ebe0a04602fedfb
stud2009E <stud2009@yandex.ru>
2024-01-29 16:40:26 +0300

    Feature/module12 tasks (#13)
    * feature: crud for user entity
    
    * move pkg entity, repo into dao
    
    * change alluser.html
    
    * fix: add check to create/update user
    
    * feature: add service for repo; change application settings to yaml
    
    * fix: setup custom 404 Not found
    
    * feature add tests for UserController
    
    * add user service tests, repository test
    
    * feature: crud for user entity
    
    * move pkg entity, repo into dao
    
    * change alluser.html
    
    * fix: add check to create/update user
    
    * feature: add service for repo; change application settings to yaml
    
    * fix: setup custom 404 Not found
    
    * feature add tests for UserController
    
    * add user service tests, repository test
    
    * add user crud functionality
    
    * add security configuration
    
    * add new user mananager; setup authority
    
    * fix: setup test with user

---
+++ module12/src/main/java/ru/edu/module12/config/Config.java
@@ -0,0 +1,11 @@
+package ru.edu.module12.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = "ru.edu.module12.dao")
+@Configuration
+@PropertySource("classpath:application.yml")
+public class Config {
+}
